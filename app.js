81fd6fc535760051aeef7450e37200aaeba0334a
baetenJ <jbaeten@ivytech.edu>
2024-03-04 14:14:14 -0500

    authentication

--- app.js
+++ app.js
@@ -2,17 +2,84 @@
 
 const express = require('express');
 var cors = require('cors');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const Course = require('./models/courses');
-const jwt = require('jwt-simple');
 const User = require('./models/users')
+const jwt = require('jwt-simple');
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const router = express.Router();
 const secret = "supersecret"
+
+
+router.post("/user", async(res, req) => {
+    if(!req.body.username || !req.body.password){
+        res.status(400).json({error: "Missing username or password"})
+    }
+    const newUser = await new User({
+        username: req.body.username,
+        password: req.body.password,
+        status: req.body.status
+    })
+    try {
+        await newUser.save()
+        res.sendStatus(201)
+    }
+    catch (err){
+        res.status(400).send(err)
+    }
+})
+
+
+// login
+router.post("/auth", async(req, res) => {
+    if(!req.body.username || !req.body.password) {
+        res.status(400).json({error: "Missing username or password"})
+        return 
+    }
+    let user = await User.findOne({username: req.body.username})
+
+    if (!user){
+        res.status(401).json({error: "Bad Username"})
+    }
+    else {
+        if (user.password != req.body.password){
+            res.status(401).json({error: "Bad Password"})
+        }
+        else {
+            username2 = user.username
+            const token = jwt.encode({username: user.username}, secret)
+            const auth = 1
+
+            res.json({
+                username2,
+                token: token,
+                auth: auth
+                })
+            }
+        }
+})
+
+router.get("/status", async(res,req) => {
+    if(!req.headers["x-auth"]){
+        return res.status(401).json({error: "Missing X-Auth"})
+    }
+    const token = req.headers["x-auth"]
+    try {
+        const decoded = jwt.decode(token ,secret)
+        let users = User.find({}, "username status")
+        res.json(users)
+    }
+    catch(ex){
+        res.status(401).json({error: "Invalid jwt"})
+    }
+})
+
 
 // grab all courses in databse
 
@@ -90,69 +157,6 @@ router.get("/courses/:id", async (req, res) => {
     }
 });
 
-
-router.post("/user", async(res, req) => {
-    if(!req.body.username || !req.body.password){
-        res.status(400).json({error: "Missing username or password"})
-    }
-    const newUser = await newUser({
-        username: req.body.username,
-        password: req.body.password,
-        status: req.body.status
-    })
-    try {
-        await newUser.save()
-        res.sendStatus(201)
-    }
-    catch (err){
-        res.status(400).send(err)
-    }
-})
-
-
-// login
-router.post("/auth", async(req, res) => {
-    if(!req.body.username || !req.body.password) {
-        res.status(400).json({error: "Missing username or password"})
-        return 
-    }
-    let user = await User.findOne({username: req.body.username})
-
-    if (!user){
-        res.status(401).json({error: "Bad Username"})
-    }
-    else {
-        if (user.password != req.body.password){
-            res.status(401).json({error: "Bad Password"})
-        }
-        else {
-            username2 = user.username
-            const token = jwt.encode({username: user.username}, secret)
-            const auth = 1
-
-            res.json({
-                username2,
-                token: token,
-                auth: auth
-                })
-            }
-        }
-})
-
-router.get("/status", async(res,req) => {
-    if(!req.headers["x-auth"]){
-        return res.status(401).json({error: "Missing X-Auth"})
-    }
-    const token = req.headers["x-auth"]
-    try {
-        const decoded = jwt.decoded(token ,secret)
-        let users = User.find({}, "username status")
-        res.json(users)
-    }
-    catch(ex){
-        res.status(401).json({error: "Invalid jwt"})
-    }
-})
 
 app.use("/api", router);
 app.listen(3000);
--- models/users.js
+++ models/users.js
@@ -3,6 +3,7 @@ const db = require("../db");
 const User = db.model("User", {
     username: {type: String, required: true},
     password: {type: String, required: true},
+    status: String
 });
 
 module.exports = User
